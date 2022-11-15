b0c90598958ff9db95faa8230fde5131d293315a
DevSenpai09 <devsenpai09@gmail.com>
2022-11-15 23:57:12 +0100

    fixed pagination

--- src/components/pagination-select/index.jsx
+++ src/components/pagination-select/index.jsx
@@ -1,14 +1,18 @@
 import SelectInput from "../../components/select-input";
+import "../../styles/pagination-select/index.css";
 
 const PaginationSelect = () => {
   const options = [10, 20, 30, 40, 50, 60, 70, 80, 90];
   return (
-    <SelectInput
-      placeholder="20"
-      options={options}
-      dropDownPos="up"
-      //   dropDownSearch
-    />
+    <div className="pagination-select">
+      <SelectInput
+        placeholder="20"
+        options={options}
+        dropDownPos="up"
+        //   dropDownSearch
+      />
+      <p className="pagination-select__text">per page</p>
+    </div>
   );
 };
 
